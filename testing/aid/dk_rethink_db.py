b80592e21047af8c428a8a8b825861f87684ab40
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2016-05-16 15:31:36 +0100

    allow the tests to pickup and use an existing rethindb which would be present in a docker link situation. This allows jenkins to control the container creation and setup/teardown of services.

--- testing/aid/dk_rethink_db.py
+++ testing/aid/dk_rethink_db.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 """
+import os
 import time
 import uuid
 import logging
-
-import requests
 
 from testing.aid.dockerbase import DockerBase
 
@@ -18,11 +17,88 @@ class DKRethinkDB(DockerBase):
     """This overrides implements the waitForReady needed to make sure rethinkdb
     is ready for use.
 
+    If RETHINKDB_PORT_28015_TCP_ADDR and RETHINKDB_PORT_28015_TCP_PORT are
+    defined in the environment, they will be used and no container started.
+    This allows test runs which link to containers started and managed
+    elsewhere.
+
+    dk_config.yaml entry::
+
+        rethinkdb:
+            image: rethinkdb
+            interface: 0.0.0.0
+            auth:
+                user:
+                password:
+            entrypoint:
+            export:
+                wait_for_port: db
+                ports:
+                    - port: 8080
+                      name: admin
+                    - port: 28015
+                      name: db
+
     """
     def __init__(self, cfg):
         """Recover our set up from the influxdb container section.
         """
         super(DKRethinkDB, self).__init__(cfg, 'rethinkdb')
+        log = get_log("DKRethinkDB.__init__")
+        self.use_env = False
+
+        if (
+            os.environ.get("RETHINKDB_PORT_28015_TCP_ADDR")
+            and
+            os.environ.get("RETHINKDB_PORT_28015_TCP_PORT")
+        ):
+            self.host = os.environ.get("RETHINKDB_PORT_28015_TCP_ADDR")
+            self.port = os.environ.get("RETHINKDB_PORT_28015_TCP_PORT")
+            log.info(
+                (
+                    "Using from environment RETHINKDB_PORT_28015_TCP_ADDR={} "
+                    "and RETHINKDB_PORT_28015_TCP_PORT={}"
+                ).format(
+                    self.host, self.port
+                )
+            )
+            # Indicate not to start a container but to connect to an
+            # exiting instance.
+            self.use_env = True
+
+    def setUp(self):
+        log = get_log("DKRethinkDB.setUp")
+
+        if self.use_env is False:
+            super(DKRethinkDB, self).setUp()
+
+        else:
+            log.warn(
+                (
+                    "Not starting a container. Using an existing running"
+                    "RethinkDB on host '{}' and port '{}'"
+                ).format(
+                    self.host,
+                    self.port,
+                )
+            )
+
+    def tearDown(self):
+        log = get_log("DKRethinkDB.tearDown")
+
+        if self.use_env is False:
+            super(DKRethinkDB, self).tearDown()
+
+        else:
+            log.warn(
+                (
+                    "Not stopping a container as I'm using an existing running"
+                    "RethinkDB on host '{}' and port '{}'"
+                ).format(
+                    self.host,
+                    self.port,
+                )
+            )
 
     def waitForReady(self):
         """Wait for the client socker to be available then attempt to create
@@ -35,11 +111,16 @@ class DKRethinkDB(DockerBase):
         db = "testreadytorolldb_{}".format(uuid.uuid4().hex)
 
         # wait for the first port to respond to connections:
-        interface = self.settings['interface']
-        name = self.settings['export']['wait_for_port']
-        ports = self.settings['export']['ports']
-        port = [p['export_port'] for p in ports if p['name'] == name]
-        port = port[0]
+        if self.use_env is False:
+            interface = self.settings['interface']
+            name = self.settings['export']['wait_for_port']
+            ports = self.settings['export']['ports']
+            port = [p['export_port'] for p in ports if p['name'] == name]
+            port = port[0]
+
+        else:
+            interface = self.host
+            port = self.port
 
         # Create a database then drop it which should test influxdb is running
         # and ready. This may fail with ConnectionError as the container is
@@ -72,5 +153,8 @@ class DKRethinkDB(DockerBase):
                 raise
 
             else:
+                # Update these now they are confirmed working:
+                self.host = interface
+                self.port = port
                 rethinkdb.db_drop(db).run(conn)
                 break
